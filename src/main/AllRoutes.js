6a98b1ff649f439981c936d715567337a0b60a7d
Alan Montgomery <alanmontgomerycoding@gmail.com>
2021-03-13 16:55:14 +0000

    Comments

--- src/main/AllRoutes.js
+++ src/main/AllRoutes.js
@@ -15,6 +15,16 @@ import TabMenu from "./TabMenu";
 import SubRoutes from "./SubRoutes";
 
 //	Array of objects representing tab pages
+//  These will be the main tabs across the app
+
+//  *   PARAMS per tab object   *
+//  isTab = true will make the tab appear
+//  default = the default tab page to open and be redirected to at "/" 
+//  NOTE: there should only be one default tab (default: true)
+//  label = the label to show with the tab
+//  component = the component related to this tab page
+//  icon = icon to show on the tab bar menu
+//  path = the path which the tab is accessible
 export const tabRoutes = [
 
     { label: "Profile", component: Tab1, icon: personOutline, path: "/tabs/tab1", default: true, isTab: true },
@@ -23,22 +33,34 @@ export const tabRoutes = [
 ];
 
 //  Array of objects representing children pages of tabs
+
+//  *   PARAMS per tab object   *
+//  isTab = should always be set to false for these
+//  component = the component related to this tab page
+//  path = the path which the tab is accessible
+
 //  These pages should be related to tab pages and be held within the same path
 //  E.g. /tabs/tab1/child
 const tabChildrenRoutes = [
 
-    { component: Settings, path: "/tabs/tab1/settings", default: false, isTab: false },
+    { component: Settings, path: "/tabs/tab1/settings", isTab: false },
 ];
 
 //  Array of objects representing sub pages
+
+//  *   PARAMS per tab object   *
+//  component = the component related to this sub page
+//  path = the path which the sub page is accessible
+
 //  This array should be sub pages which are not directly related to a tab page
 //  E.g. /child
 const subPageRoutes = [
 
-    { component: InboxItem, path: "/inbox/:id", default: false, isTab: false },
+    { component: InboxItem, path: "/inbox/:id" },
 ];
 
-const tabsAndChildrenRoutes = [ ...tabRoutes, ...tabChildrenRoutes ]
+//  Let's combine these together as they need to be controlled within the same IonRouterOutlet
+const tabsAndChildrenRoutes = [ ...tabRoutes, ...tabChildrenRoutes ];
 
 //  Render sub routes
 export const SubPages = () => ( <SubRoutes routes={ subPageRoutes } /> );
